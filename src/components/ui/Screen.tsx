f988ea9c5f98e8730742304dbb1aa62db398778d
TeeJay-EasyPay <tarikjehangir@gmail.com>
2026-05-03 12:59:03 +0100

    Add hamburger dropdown menu to shared screen layout

--- src/components/ui/Screen.tsx
+++ src/components/ui/Screen.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { StyleSheet, View, ViewStyle } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
+import { AppDropdownMenu } from "../navigation/AppDropdownMenu";
 import { AppMenu } from "../navigation/AppMenu";
 import { colors, spacing } from "../../theme";
 
@@ -13,7 +14,10 @@ type ScreenProps = {
 export function Screen({ children, style }: ScreenProps) {
   return (
     <SafeAreaView style={[styles.container, style]}>
+      <AppDropdownMenu />
+
       <View style={styles.content}>{children}</View>
+
       <AppMenu />
     </SafeAreaView>
   );
