5359f9bb0729251feea93dcc4134ff3d6af77aa8
TeeJay-EasyPay <tarikjehangir@gmail.com>
2026-05-03 20:33:49 +0100

    Add auth provider to app layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,15 +2,18 @@ import "react-native-get-random-values";
 
 import { Stack } from "expo-router";
 
+import { AuthProvider } from "../src/state/AuthContext";
 import { TransferProvider } from "../src/state/TransferContext";
 import { WalletProvider } from "../src/state/WalletContext";
 
 export default function Layout() {
   return (
-    <WalletProvider>
-      <TransferProvider>
-        <Stack screenOptions={{ headerShown: false }} />
-      </TransferProvider>
-    </WalletProvider>
+    <AuthProvider>
+      <WalletProvider>
+        <TransferProvider>
+          <Stack screenOptions={{ headerShown: false }} />
+        </TransferProvider>
+      </WalletProvider>
+    </AuthProvider>
   );
 }
