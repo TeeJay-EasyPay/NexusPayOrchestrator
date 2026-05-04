aed6214f996ad8509a37744777853157873ed583
TeeJay-EasyPay <tarikjehangir@gmail.com>
2026-05-04 14:12:19 +0100

    Add saved recipient type

---
+++ src/types/recipient.ts
@@ -0,0 +1,19 @@
+import { Currency, PayoutMethod } from "./transfer";
+
+export interface SavedRecipient {
+  id: string;
+  name: string;
+  firstName?: string;
+  middleName?: string;
+  surname?: string;
+  country: string;
+  currency: Currency;
+  payoutMethod: PayoutMethod;
+  bankName?: string;
+  bankCode?: string;
+  accountNumber?: string;
+  mobileWalletProvider?: string;
+  mobileNumber?: string;
+  lastUsedAt: number;
+  createdAt: number;
+}
