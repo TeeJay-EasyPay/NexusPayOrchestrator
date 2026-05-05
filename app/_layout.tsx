68c6e2c23bb4ee97935ddb547e9fe19505aea8cd
TeeJay-EasyPay <tarikjehangir@gmail.com>
2026-05-05 01:29:10 +0100

    Wrap app with device unlock provider

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,19 +4,22 @@ import { Stack } from "expo-router";
 
 import { AuthGate } from "../src/components/auth/AuthGate";
 import { AuthProvider } from "../src/state/AuthContext";
+import { DeviceUnlockProvider } from "../src/state/DeviceUnlockContext";
 import { TransferProvider } from "../src/state/TransferContext";
 import { WalletProvider } from "../src/state/WalletContext";
 
 export default function Layout() {
   return (
     <AuthProvider>
-      <WalletProvider>
-        <TransferProvider>
-          <AuthGate>
-            <Stack screenOptions={{ headerShown: false }} />
-          </AuthGate>
-        </TransferProvider>
-      </WalletProvider>
+      <DeviceUnlockProvider>
+        <WalletProvider>
+          <TransferProvider>
+            <AuthGate>
+              <Stack screenOptions={{ headerShown: false }} />
+            </AuthGate>
+          </TransferProvider>
+        </WalletProvider>
+      </DeviceUnlockProvider>
     </AuthProvider>
   );
 }
